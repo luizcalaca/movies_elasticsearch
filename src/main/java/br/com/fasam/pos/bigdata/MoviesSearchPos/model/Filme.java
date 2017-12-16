8b3a4ade45a64d71bc21528f638f8d8fcb6f734d
Luiz Fernando Calaça Silva <luizcalaca@gmail.com>
2017-12-16 00:31:30 -0200

    Feito import de Data

--- src/main/java/br/com/fasam/pos/bigdata/MoviesSearchPos/model/Filme.java
+++ src/main/java/br/com/fasam/pos/bigdata/MoviesSearchPos/model/Filme.java
@@ -1,6 +1,7 @@
 package br.com.fasam.pos.bigdata.MoviesSearchPos.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class Filme implements Serializable{
 	private static final long serialVersionUID = 2399028824723797630L;
